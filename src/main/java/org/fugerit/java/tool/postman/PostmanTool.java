93b8b6b8e6dbb802d8b28e2ad8a6fba99b055dad
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2024-03-18 20:11:12 +0100

    Main class

---
+++ src/main/java/org/fugerit/java/tool/postman/PostmanTool.java
@@ -0,0 +1,18 @@
+package org.fugerit.java.tool.postman;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class PostmanTool {
+
+    public static final String ARG_FUN = "fun";
+
+    public static void main( String[] args ) {
+        try {
+
+        } catch (Exception e) {
+            log.error( "Error : "+e, e );
+        }
+    }
+
+}
